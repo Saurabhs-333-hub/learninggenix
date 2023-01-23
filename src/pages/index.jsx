3784e6f3646c9a567bf99a4de73aabe37f32cb5e
saurabh soni <sonisaurabhsoni333@gmail.com>
2023-01-23 06:16:23 +0530

    commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,11 +5,14 @@ const auth = getAuth();
 const Index = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [data, setData] = useState('')
+    const [emailsignIn, setEmailsignIn] = useState('')
+    const [passwordsignIn, setPasswordsignIn] = useState('')
+    // const [data, setData] = useState('')
     const handleSubmit = () => {
         createUserWithEmailAndPassword(auth, email, password).then((res) => {
             console.log(res)
-            setData(res.user.email)
+            { localStorage.setItem('user', res.user.email) }
+            // setData(res.user.email)
         }).catch((err) => {
             console.log(err)
         })
@@ -18,7 +21,8 @@ const Index = () => {
         signInWithEmailAndPassword(auth, email, password).then((res) => {
             console.log(res)
             { localStorage.setItem('user', res.user.email) }
-            setData(res.user.email)
+            // setData(res.user.email)
+            window.location.assign('/profile')
         }).catch((err) => {
             console.log(err)
         })
@@ -27,22 +31,23 @@ const Index = () => {
         signOut(auth).then((res) => {
             console.log("SignOut")
             { localStorage.removeItem('user') }
-            setData("No User")
+            // setData("No User")
         }).catch((err) => {
             console.log(err)
         })
     }
     return (
         <div>
-            {/* <input autoFocus type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} /> */}
+            <input autoFocus type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
             <div>
                 {localStorage.getItem('user')}
                 SignInSuccessfull
             </div>
-            <button onClick={handleSignin}>Submit</button>
-            <input autoFocus type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
-            <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleSubmit}>SignUp</button>
+            <button onClick={handleSignin}>SignIn</button>
+            <input autoFocus type="text" value={emailsignIn} onChange={(e) => setEmailsignIn(e.target.value)} />
+            <input type="text" value={passwordsignIn} onChange={(e) => setPasswordsignIn(e.target.value)} />
             <button onClick={handleSignOut}>SignOut</button>
         </div>
     )
